26c051528d7efb74037f685e221a17ebac2b1156
Jila <jilakhaghani@gmail.com>
2021-06-11 10:55:56 +1000

    Add carMakeApi tests

---
+++ tests/Feature/CarMakeApiTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class CarMakeApiTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function test_create_car_make_api()
+    {
+        $this->json('POST', '/api/car-make', ['make' => 'test car make'])
+            ->assertStatus(200)
+            ->assertJsonStructure(['data' => ['value', 'label']]);
+    }
+
+    public function test_car_make_missing_input()
+    {
+        $this->json('POST', '/api/car-make', ['make' => ''])
+            ->assertStatus(400)
+            ->assertJsonStructure(['message']);
+    }
+
+    public function test_get_cars()
+    {
+        $this->json('GET', '/api/car-make')
+            ->assertStatus(200)
+            ->assertJsonStructure(['*' => ['value', 'label']]);
+    }
+}
